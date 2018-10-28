96a0ba4d45e891ec5b167117bf5e31f31d0ff90f
vsujananth <vsujananthprojectadio@gmail.com>
2018-10-28 11:57:31 +0530

    Completed Storyboard design

---
+++ MyContacts/MyContacts/ViewController/ContactsViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ContactsViewController.swift
+//  MyContacts
+//
+//  Created by Sujananth on 10/27/18.
+//  Copyright © 2018 sujananth. All rights reserved.
+//
+
+import UIKit
+
+class ContactsViewController: UIViewController {
+    
+    @IBOutlet weak var contactsSearchBar: UISearchBar!
+    @IBOutlet weak var contactsListTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
